fdc2130e20ec407b71fbb0fa4fed4112cfa33ce0
sunflower0054 <sunflower0054@naver.com>
2026-03-25 03:45:43 +0900

    웹 브라우저 외형 잡기

--- src/main/java/com/office/monitoring/index/IndexController.java
+++ src/main/java/com/office/monitoring/index/IndexController.java
@@ -10,4 +10,32 @@ public class IndexController {
     public String index() {
         return "index/index";
     }
+
+    @GetMapping("/index")  // 추가
+    public String indexPage() {
+        return "index/index";
+    }
+
+    @GetMapping("/camera")  // 추가
+    public String camera() {
+        return "camera/camera";
+    }
+
+    @GetMapping("/event")  // 오타 수정 + 경로 수정
+    public String event() {
+        return "event/event";
+    }
+
+    @GetMapping("/status")  // 경로 수정
+    public String status() {
+        return "status/status";
+    }
+
+    @GetMapping("setting")  // 경로 수정
+    public String setting() {
+        return "setting/setting";
+    }
+
+
+
 }
