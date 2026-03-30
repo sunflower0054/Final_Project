69227c909fc03f57d729bee0f001e5afb349e904
Anim1048576 <darekax10@gmail.com>
2026-03-30 16:58:00 +0900

    asdf

--- src/test/java/com/office/monitoring/MonitoringApplicationTests.java
+++ src/test/java/com/office/monitoring/MonitoringApplicationTests.java
@@ -6,7 +6,7 @@ import com.office.monitoring.member.Role;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
