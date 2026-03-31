e68ee7fa9a516bfb737b3e3a712c31530e54c902
Anindeyo <darekax10@gmail.com>
2026-03-31 14:06:37 +0900

    test: align /setting access tests with security policy

--- src/test/java/com/office/monitoring/MonitoringApplicationTests.java
+++ src/test/java/com/office/monitoring/MonitoringApplicationTests.java
@@ -75,15 +75,22 @@ class MonitoringApplicationTests {
     }
 
     @Test
-    void FAMILY_권한_setting_접근시_403() throws Exception {
+    void FAMILY_로그인_setting_접근시_200() throws Exception {
         mockMvc.perform(get("/setting").with(user("user").roles("FAMILY")))
-            .andExpect(status().isForbidden());
+            .andExpect(status().isOk());
     }
 
     @Test
     void ADMIN_권한_setting_접근시_200() throws Exception {
         mockMvc.perform(get("/setting").with(user("admin").roles("ADMIN")))
             .andExpect(status().isOk());
+    }
+
+    @Test
+    void 비로그인_사용자_setting_접근시_로그인페이지로_리다이렉트() throws Exception {
+        mockMvc.perform(get("/setting"))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrl("/member/login"));
     }
 
     @Test
