1ca420ba13cd7415bcafbd24a930e6be84911182
Anindeyo <darekax10@gmail.com>
2026-04-02 18:56:14 +0900

    Prevent duplicate ai_settings insert on resident registration

--- src/main/java/com/office/monitoring/aiSettings/AiSettingsRepository.java
+++ src/main/java/com/office/monitoring/aiSettings/AiSettingsRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AiSettingsRepository extends JpaRepository<AiSettings, Long> {
 
+    boolean existsByResidentId(Long residentId);
+
     // resident_id로 설정값 조회
     AiSettings findByResidentId(Long residentId);
 }
